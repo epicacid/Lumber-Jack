ba4672d00ea3e17b227cf74184e1395800fc17bf
epicacid <noah@Noahs-MacBook-Pro.local>
2016-10-25 11:50:41 +0200

    Camera

--- Assets/Noah/Scripts/Camera.cs
+++ Assets/Noah/Scripts/Camera.cs
@@ -2,17 +2,11 @@
 using System.Collections;
 
 public class Camera : MonoBehaviour {
-
-
-
-	// Use this for initialization
-	void Start () {
-	
+	public float horizontalSpeed = 2.0F;
+	public float verticalSpeed = 2.0F;
+	void Update() {
+		float h = horizontalSpeed * Input.GetAxis("Mouse X");
+		float v = verticalSpeed * Input.GetAxis("Mouse Y");
+		transform.Rotate(v, h, 0);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	
-}
 }
