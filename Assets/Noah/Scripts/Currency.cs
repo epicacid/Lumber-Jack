ec1512e18a90c37cc425a240403f7f159fc42245
epicacid <noah@Noahs-MacBook-Pro.local>
2016-10-28 09:41:22 +0200

    Currency + Upgrade

--- Assets/Noah/Scripts/Currency.cs
+++ Assets/Noah/Scripts/Currency.cs
@@ -3,10 +3,25 @@ using System.Collections;
 
 public class Currency : MonoBehaviour {
 	[SerializeField]
-	private float curCurrency;
+	private int curCurrency;
+	[SerializeField]
+	private int sellingValue = 25;
 
 	void Start(){
 		curCurrency = 100;
 	}
-
+	public int CurCurrency{
+		get{
+			return curCurrency;
+		}set{
+			curCurrency = value;
+		}
+	}
+	public int SellingValue{
+		get{
+			return sellingValue;
+		}set{
+			sellingValue = value;
+		}
+	}
 }
--- Assets/Noah/Scripts/PlayerStats.cs
+++ Assets/Noah/Scripts/PlayerStats.cs
@@ -4,19 +4,42 @@ using System.Collections;
 public class PlayerStats : MonoBehaviour {
 
 	[SerializeField]
-	private float playerChopSpeed = 10.0f;
+	private int playerChopSpeed = 10;
 	[SerializeField]
 	private int woodHold = 10;
 	[SerializeField]
 	private int rockHold = 10;
 	[SerializeField]
-	private float movSpeed = 10.0f;
+	private int movSpeed = 10;
 
-	public float PlayerChopSpeed{
+	public int PlayerChopSpeed{
 		get{
 			return playerChopSpeed;
 		}set{
 			playerChopSpeed = value;
 		}
 	}
+	public int WoodHold{
+		get{
+			return woodHold;
+		}set{
+			woodHold = value;
+		}
+	}
+	public int RockHold{
+		get{
+			return rockHold;
+		}set{
+			rockHold = value;
+		}
+	}
+	public int MovSpeed{
+		get{
+			return movSpeed;
+		}set{
+			movSpeed = value;
+		}
+	}
 }
+
+
