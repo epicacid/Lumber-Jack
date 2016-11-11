8b55fc3494dc180f9f2e2fbbd4f89dfe7a8ed062
nielsvdmarel <nielsvdmarel@gmail.com>
2016-11-11 18:27:14 +0100

    last

--- Assets/Jeroen/Gathering.cs
+++ Assets/Jeroen/Gathering.cs
@@ -2,68 +2,93 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Gathering : MonoBehaviour {
+public class Gathering : MonoBehaviour
+{
 
 
 
-	public float hout = 0f;
-	public float steen = 0f;
-	Camera camera;
-	private Hout Wood;
-	private Steen Stone;
-	private float ClickedHout = 0f;
-	private float ClickedStone = 0f;
+    public float hout = 0f;
+    public float steen = 0f;
+    Camera camera;
+    private Hout Wood;
+    private Steen Stone;
+    private float ClickedHout = 0f;
+    private float ClickedStone = 0f;
+    public GameObject Axe;
+    public GameObject Pickaxe;
 
 
 
-	// Use this for initialization
-	void Start () {
-		camera = GetComponent<Camera>();
-		Wood = GameObject.FindGameObjectWithTag ("Wood").GetComponent<Hout> ();
-		Stone = GameObject.FindGameObjectWithTag ("Stone").GetComponent<Steen> ();
+    // Use this for initialization
+    void Start()
+    {
+        //camera = GetComponent<Camera>();
 
+    }
 
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            //print("I'm looking at " + hit.transform.name);
 
-	// Update is called once per frame
-	void Update () {
-		Ray ray = camera.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));
-		RaycastHit hit;
-		if (Physics.Raycast(ray, out hit)){
-			print("I'm looking at " + hit.transform.name);
+            if (hit.transform.CompareTag("Stone"))
+            {
+                Pickaxe.SetActive(true);
+                Axe.SetActive(false);
+                if (hit.collider.CompareTag("Stone"))
+                {
+                    
 
-			if(hit.collider.CompareTag ("Stone"))
-			{
-				if (Input.GetKeyDown (KeyCode.Mouse0)) {
-					ClickedStone += 1f;
-					Debug.Log (ClickedStone);
-					if (ClickedStone == 5) {
-						steen += 1f;
-						--Stone.Health;
-						ClickedStone = 0;
-						Debug.Log (ClickedStone);
-					}
+                    if (Input.GetKeyDown(KeyCode.Mouse0))
+                    {
+                        Stone = hit.transform.GetComponent<Steen>();
 
-				}
+                        ClickedStone += 1f;
+                        Debug.Log(ClickedStone);
+                        if (ClickedStone == 5)
+                        {
+                            steen += 1f;
+                            --Stone.Health;
+                            ClickedStone = 0;
+                            Debug.Log(ClickedStone);
+                        }
 
-			}
-			if(hit.collider.CompareTag ("Wood"))
-			{
-				if (Input.GetKeyDown (KeyCode.Mouse0)) {
-					ClickedHout += 1f;
-					Debug.Log (ClickedHout);
-					if (ClickedHout == 5) {
-						hout += 1f;
-						--Wood.Health;
-						ClickedHout = 0;
-						Debug.Log (ClickedHout);
-					}
-				}
+                    }
 
-			}
-		}
+                }
+            }
 
-	}
+            if (hit.transform.CompareTag("Wood"))
+            {
+                Pickaxe.SetActive(false);
+                Axe.SetActive(true);
+                if (hit.collider.CompareTag("Wood"))
+                {
+                   
+
+                    if (Input.GetKeyDown(KeyCode.Mouse0))
+                    {
+                        Wood = hit.transform.parent.GetComponent<Hout>();
+                        ClickedHout += 1f;
+                        Debug.Log(hit.collider.name +  "   time+: " + ClickedHout);
+                        if (ClickedHout >= 5)
+                        {
+                            hout += 1f;
+                            --Wood.Health;
+                            ClickedHout = 0;
+                            Debug.Log(ClickedHout);
+                        }
+                    }
+
+                }
+            }
+
+        }
+    }
 }
 
 
